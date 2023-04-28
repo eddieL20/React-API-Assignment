05ea60158da6a219697408ae5086d4a4eace86fc
Eduardo Lopez <eddie56.el@gmail.com>
2023-04-28 00:50:57 -0700

    add brewery info

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
+const App = () => {
+  const [items, setItems] = React.useState([]);
+  const [isDataLoaded, setIsDataLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch("https://api.openbrewerydb.org/v1/breweries?by_city=san_diego")
+      .then((Response) => Response.json())
+      .then((json) => {
+        setItems(json);
+        setIsDataLoaded(true);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Breweries in San Diego I'd like to visit</h1>
+      {items.map((item) => (
+        <ul key={item.name}>
+          <li className="name">{item.name}</li>
+          <li>
+            <span>Address: </span> {item.address_1}
+          </li>
+          <li>
+            <span>Phone: </span> {item.phone}
+          </li>
+          <li>
+            <span>Coordinates: </span> {item.latitude}, {item.longitude}
+          </li>
+          <li>
+            <span>Website: </span> <a href={item.website_url}>{item.website_url}</a>
+          </li>
+        </ul>
+      ))}
     </div>
   );
-}
-
+};
 export default App;
